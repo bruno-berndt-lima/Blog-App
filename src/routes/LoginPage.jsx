1c0108e95175029216e9004d7fd8b409ee369703
Bruno Berndt Lima <brunolima674@gmail.com>
2025-05-27 14:49:42 -0300

    Add MainCategories & FeaturePosts to homepage

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react"
 
 const LoginPage = () => {
     return (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex items-center justify-center h-[calc(100vh-50px)]">
             <SignIn signUpUrl="/register"/>
         </div>
     )
