db8c932a3adc90aded9fe561b0fdea01206b85a2
Bruno Berndt Lima <brunolima674@gmail.com>
2025-05-28 15:31:15 -0300

    Add backend config

---
+++ backend/models/post.model.js
@@ -0,0 +1,41 @@
+import {Schema} from "mongoose"
+import mongoose from "mongoose"
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: Schema.Types.ObjectId, 
+        ref: "User", 
+        required: true,
+    },
+    img: {
+        type: String,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    visit: {
+        type: Boolean,
+        default: 0,
+    }, 
+  }, 
+  {timestamps: true}
+);
+
+export default mongoose.model("Post", postSchema);
